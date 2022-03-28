6a84d2a916f254253758322bfb525e35327b0153
Alejandro P <lalebot@users.noreply.github.com>
2022-03-28 09:09:34 -0300

    Update rstudio.sh

--- rstudio.sh
+++ rstudio.sh
@@ -1,7 +1,7 @@
 # update indices
 apt update -qq &&
 # install two helper packages we need
-apt install --no-install-recommends software-properties-common dirmngr &&
+apt install --no-install-recommends software-properties-common dirmngr -Y &&
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
@@ -10,4 +10,4 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" &&
 
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2022.02.1-461-amd64.deb &&
-apt-get install -f ./rstudio-2022.02.1-461-amd64.deb --assume-yes
+apt install -f ./rstudio-2022.02.1-461-amd64.deb -Y

c53a6832814f378de6bb700a2f53e16b55171954
Ale <pistilli.alejandro@gmail.com>
2024-05-15 10:58:21 -0300

    curl_check

---
+++ curl_check.sh
@@ -0,0 +1,23 @@
+curl_check ()
+{
+  echo "Checking for curl..."
+  if command -v curl > /dev/null; then
+    echo "Detected curl..."
+  else
+    echo "Installing curl..."
+    apt-get install -q -y curl
+    if [ "$?" -ne "0" ]; then
+      echo "Unable to install curl! Your base system has a problem; please check your default OS's package repositories because curl should work."
+      echo "Repository installation aborted."
+      exit 1
+    fi
+  fi
+}
+
+
+main ()
+{
+  curl_check
+}
+
+main
